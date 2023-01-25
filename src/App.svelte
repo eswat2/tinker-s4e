2b90695cbe6665487e632f9ce9f22ba99c748df6
Richard Hess <eswat2@gmail.com>
2023-01-24 18:36:34 -0800

    add Header component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
   import ColorPicker from "./components/ColorPicker.svelte"
   import Eswat2 from "./components/Eswat2.svelte"
+  import Header from "./components/Header.svelte"
   import Sampler from "./components/Sampler.svelte"
   import Values from "./components/Values.svelte"
 </script>
 
 <main class="ds1-main">
+  <Header label="tinker s4e" />
   <Eswat2 />
   <Sampler />
   <ColorPicker />
---
+++ src/components/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let label
+</script>
+
+<h1
+  class="text-clrs-red mr-0 ml-0 mt-11 mb-11 text-center text-6xl font-thin uppercase"
+>
+  {label}
+</h1>
